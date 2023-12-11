09a1308a4864c0a018967399dc336ffeebdc793e
ajuvi <102821845+ajuvi@users.noreply.github.com>
2023-12-11 10:55:38 +0100

    Crear projecte

---
+++ Recursivitat/Recursivitat/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Recursivitat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void Menu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
